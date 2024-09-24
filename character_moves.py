a6335e5d451dfff7b61ec388d9070ca0795bd8b9
hyeon45 <gahyeon4585@daum.net>
2024-09-24 11:49:57 +0900

    반복문 뼈대 완성 / print로 확인하는 용도

--- character_moves.py
+++ character_moves.py
@@ -1,11 +1,23 @@
 from pico2d import *
+import math
 
 open_canvas()
 
 grass = load_image('grass.png')
 character = load_image('character.png')
 
-# 여기를 채우세요.
+def run_circle():
+    print('CIRCLE')
+    pass
+
+def run_rectangle():
+    print('RECTANGLE')
+    pass
+
+while (True):
+    run_circle()
+    run_rectangle()
+    
 
 close_canvas()
 
