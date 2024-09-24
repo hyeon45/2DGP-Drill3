b190dd60c182ceb67f4d23a1860b0405c4194971
hyeon45 <gahyeon4585@daum.net>
2024-09-24 11:56:32 +0900

    화면에 캐릭터가 출력이 되는지 확인

--- character_moves.py
+++ character_moves.py
@@ -8,7 +8,10 @@ character = load_image('character.png')
 
 def run_circle():
     print('CIRCLE')
-    pass
+
+    clear_canvas()
+    character.draw_now(400, 30)
+    delay(0.1)
 
 def run_rectangle():
     print('RECTANGLE')
@@ -17,6 +20,7 @@ def run_rectangle():
 while (True):
     run_circle()
     run_rectangle()
+    break #빠르게 확인하는 용도
     
 
 close_canvas()
