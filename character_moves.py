5cdfa0b1faa4e5db02612a5969e3e4ad301e142d
hyeon45 <gahyeon4585@daum.net>
2024-09-24 12:12:19 +0900

    원 운동 구현 완료

--- character_moves.py
+++ character_moves.py
@@ -9,9 +9,16 @@ character = load_image('character.png')
 def run_circle():
     print('CIRCLE')
 
-    clear_canvas()
-    character.draw_now(400, 30)
-    delay(0.1)
+    r, cx, cy = 300, 800//2, 600//2
+
+    for degree in range(0, 360, 3):
+        theta = math.radians(degree)
+        x = r * math.cos(theta) + cx
+        y = r * math.sin(theta) + cy
+    
+        clear_canvas()
+        character.draw_now(x, y)
+        delay(0.1)
 
 def run_rectangle():
     print('RECTANGLE')
